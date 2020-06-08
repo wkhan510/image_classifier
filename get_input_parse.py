a0b8131f4ee6a40916a0d97af35c89f1e7a4f559
wkhan510 <63086071+wkhan510@users.noreply.github.com>
2020-06-08 00:36:31 -0700

    Create get_input_parse.py

---
+++ get_input_parse.py
@@ -0,0 +1,29 @@
+# Imports python modules
+import argparse
+
+def get_input_args():
+    
+    # Create Parse using ArgumentParser
+    parser = argparse.ArgumentParser(description = 'image classification')
+    
+    
+    # command line arguments as mentioned above using add_argument() from ArguementParser method
+    parser.add_argument('--data_dir', type = str, default="./flowers/",
+                                 help = 'path to the flowers images')
+    parser.add_argument('--gpu', action = "store", default='gpu',
+                                 help = 'selecting gpu / cpu')
+    parser.add_argument('--arch',type= str, default = 'vgg16',
+                                 help = 'path to architecture moduel')
+    parser.add_argument('--save_dir', type = str, default='checkpoint.pth',
+                                 help = ' path to save checkpoint')
+    parser.add_argument('--cat_name', type = str, default='cat_to_name.json', 
+                                 help='Path of Mapping flower Name')
+    parser.add_argument('--epochs', type = int, default = 1,
+                                 help='Number of Epochs')
+    parser.add_argument('--learning_rate', type = int, default = 0.003,
+                                 help='learning rate')
+    parser.add_argument('--dropout', type = int, default = 0.2,
+                                 help='dropout')                        
+    
+    
+    return parser.parse_args()
